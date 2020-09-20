0b8591f98107412b0baf25459f9b3bfb8a0e3aee
Ahmad Rifa'i <arifai209@gmail.com>
2020-09-20 15:12:25 +0700

    Enhancement Segment

--- app/Filters/Segment.php
+++ app/Filters/Segment.php
@@ -10,10 +10,13 @@ class Segment implements FilterInterface
 {
     public function before(RequestInterface $request, $arguments = null)
     {
-        // Jika jumlah segment uri >= 2 maka segment ke 2
-        // diganti dengan karakter `/`
+        // Jika jumlah segment uri >= 3 maka segment ke 3
+        // redirect to path `/` dan jika total segment >= 2
+        // dan segment ke 2 adalah `index` maka redirect to path `/`
         $uri = service('uri');
-        if ($uri->getTotalSegments() >= 2) {
+        if ($uri->getTotalSegments() >= 3) {
+            $segment = '/';
+        } elseif ($uri->getTotalSegments() >= 2 && $uri->getSegment(2) == 'index') {
             $segment = '/';
 
             return redirect()->to($segment);
