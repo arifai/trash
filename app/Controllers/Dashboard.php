d2707648170717946c6fa8e16bbd116f998e3bc7
Ahmad Rifa'i <arifai209@gmail.com>
2020-09-25 13:11:10 +0700

    Count users, trash in, trash out in dashboard page

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -2,12 +2,25 @@
 
 namespace App\Controllers;
 
+use App\Models\Trash;
+use App\Models\User;
+
 class Dashboard extends BaseController
 {
+    public function __construct()
+    {
+        $this->UserModel = new User();
+        $this->TrashModel = new Trash();
+    }
+
     public function index()
     {
+        $users = $this->UserModel;
+        $trashes = $this->TrashModel;
         $data = [
             'title' => 'Dashboard',
+            'users' => $users,
+            'trashes' => $trashes
         ];
 
         return view('dashboard/index', $data);
